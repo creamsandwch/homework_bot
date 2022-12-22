d61d5b3cca4794c7983ab52b37b6dc6afdec7ebd
creamsandwch <neuronrus1996@gmail.com>
2022-12-22 12:17:27 +0500

    custom exception

---
+++ exceptions.py
@@ -0,0 +1,4 @@
+class StatusNotUpdated(Exception):
+    """При запросе к API яндекс.Домашки не был получен новый статус."""
+
+    pass
--- homework.py
+++ homework.py
@@ -8,6 +8,8 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
+from exceptions import StatusNotUpdated
+
 
 log_format = (
     '%(asctime)s, %(levelname)s, %(funcName)s, '
@@ -18,15 +20,15 @@ formatter = logging.Formatter(
 )
 
 console_logger = logging.StreamHandler()
-console_logger.setLevel(logging.INFO)
+console_logger.setLevel(logging.DEBUG)
 
 file_logger = logging.FileHandler('logs.log')
-file_logger.setLevel(logging.DEBUG)
+file_logger.setLevel(logging.INFO)
 
 logging.basicConfig(
     handlers=(console_logger, file_logger),
     format=log_format,
-    level=logging.DEBUG,
+    level=logging.ERROR,
 )
 
 
@@ -115,8 +117,7 @@ def check_response(response: dict) -> list:
     homeworks = response.get('homeworks')
 
     if not homeworks:
-        logging.debug('В ответе от API нет новых статусов домашки.')
-        raise Exception('В ответе от API нет новых статусов домашки.')
+        raise StatusNotUpdated('В ответе от API нет новых статусов домашки.')
     logging.info(
         'Из ответа API получен список ДЗ '
         f'из {len(homeworks)} объектов.'
@@ -162,8 +163,11 @@ def main():
             ya_api_response = get_api_answer(timestamp)
             last_homework = check_response(ya_api_response)
             message = parse_status(last_homework)
+        except StatusNotUpdated as error:
+            logging.debug(f'{error}')
+            message = f'{error}'
         except Exception as error:
-            logging.info(f'Статус проверки: {error}')
+            logging.error(f'Сбой: {error}')
             message = f'{error}'
         finally:
             if message != cached_message:
