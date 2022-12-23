d246927fdd5188c9586ec29e8bbea7a593374c4f
creamsandwch <neuronrus1996@gmail.com>
2022-12-23 11:54:05 +0500

    outsourced HOMEWORK_VERDICTS constant to settings

--- homework.py
+++ homework.py
@@ -45,11 +45,7 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
+HOMEWORK_VERDICTS = settings.HOMEWORK_VERDICTS
 
 
 def check_tokens():
